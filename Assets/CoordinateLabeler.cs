c2744b94965cc0a5c2d456a95beeb19130c902bd
Blair Donachie <blair.donachie@gmail.com>
2024-04-20 17:51:45 +0100

    Inital Commit (Lecture before currency part1)

---
+++ Assets/CoordinateLabeler.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+[ExecuteAlways]
+public class CoordinateLabeler : MonoBehaviour
+{
+    [SerializeField] Color defaultColor = Color.black;
+    [SerializeField] Color blockedColor = Color.grey;
+
+    TextMeshPro label;
+    Vector2Int coordinates = new Vector2Int();
+
+    Waypoint waypoint;
+
+    private void Awake()
+    {
+        label = GetComponent<TextMeshPro>();
+        label.enabled = false;
+        waypoint = GetComponentInParent<Waypoint>();
+        DisplayCoordinates();
+    }
+
+    void Update()
+    {
+        //Will only work in edit mode
+        if (!Application.isPlaying)
+        {
+            DisplayCoordinates();
+            UpdateObjectName();
+        }
+
+        ToggleLabels();
+        ColorCoordinates();
+    }
+    void ColorCoordinates()
+    {
+        if (waypoint.IsPlaceable)
+        {
+            label.color = defaultColor;
+        }
+        else 
+        {
+            label.color = blockedColor; 
+        }
+    }
+    void ToggleLabels()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            label.enabled = !label.IsActive();
+        }
+    }
+
+    void DisplayCoordinates()
+    {
+        coordinates.x = Mathf.RoundToInt( transform.parent.position.x / UnityEditor.EditorSnapSettings.move.x );
+        coordinates.y = Mathf.RoundToInt(transform.parent.position.z / UnityEditor.EditorSnapSettings.move.z );
+
+        label.text = coordinates.x + "," + coordinates.y;
+    }
+    void UpdateObjectName()
+    {
+        transform.parent.name = coordinates.ToString();
+    }
+}
---
+++ Assets/Waypoint.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+    [SerializeField] bool isPlaceable;
+    [SerializeField] GameObject towerPrefab;
+    public bool IsPlaceable { get { return isPlaceable; } }
+
+    void OnMouseUp()
+    {
+        if (isPlaceable)
+        {
+            Instantiate(towerPrefab, transform.position, Quaternion.identity);
+            isPlaceable = false;
+        }
+    }
+}
