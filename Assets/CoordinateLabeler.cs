1c60cca2ca26adaa961238a3a36d261b92b0b941
Blair Donachie <blair.donachie@gmail.com>
2024-04-27 19:13:47 +0100

    Review and Reflect

--- Assets/CoordinateLabeler.cs
+++ Assets/CoordinateLabeler.cs
@@ -28,7 +28,6 @@ public class CoordinateLabeler : MonoBehaviour
         {
             DisplayCoordinates();
             UpdateObjectName();
-            label.enabled = true;
         }
 
         ToggleLabels();
